627316fd38cdf0f6f8699d840490c0f9e7bb7791
Mikhirev Mikhail <mixirev99@gmail.com>
2023-04-25 22:21:21 +0300

    init

---
+++ main.py
@@ -0,0 +1,18 @@
+import math
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    a=3
+    print(f'Hi, {name}'," ", a)  # Press Ctrl+F8 to toggle the breakpoint.
+
+def pr_return():
+    return 1
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+    print("hello")
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
