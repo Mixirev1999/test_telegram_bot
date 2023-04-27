05c2ba9b5389db632bfe91580c165ea7c6aa1314
Mikhirev Mikhail <mixirev99@gmail.com>
2023-04-27 18:09:35 +0300

    start telegram bot

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import math
 import requests
-
+import pprint
 
 if __name__ == '__main__':
-    api_url = 'http://api.open-notify.org/iss-now.json'
+    api_url = 'https://api.telegram.org/bot6205359567:AAG3fGWI5-tSkkceRU6BgO4MZWncdesrBAk/sendMessage?chat_id=731057670&text=Ну ты и Миша!'
 
     response = requests.get(api_url)  # Отправляем GET-запрос и сохраняем ответ в переменной response
 
     if response.status_code == 200:  # Если код ответа на запрос - 200, то смотрим, что пришло в ответе
-        print(response.text)
+        pprint.pprint(response.text)
     else:
-        print(response.status_code)
+        pprint.pprint(response.text)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
