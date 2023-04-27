4a66a6f3f2b33bda5ebdc4f6cc0250dae7e327ea
Mikhirev Mikhail <mixirev99@gmail.com>
2023-04-27 19:00:15 +0300

    start telegram bot(2)

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,10 +1,15 @@
 import requests
 import time
+import random
 
 
 API_URL: str = 'https://api.telegram.org/bot'
 BOT_TOKEN: str = '6205359567:AAG3fGWI5-tSkkceRU6BgO4MZWncdesrBAk'
-TEXT: str = 'Ну и что!'
+API_CATS_URL: str = 'https://api.thecatapi.com/v1/images/search'
+API_WOOFS_URL: str ='https://random.dog/woof.json'
+API_FOX_URL: str ='https://randomfox.ca/floof/'
+ERROR_TEXT: str = 'Здесь должна была быть картинка с котиком :('
+TEXT: str = 'Смотри!'
 MAX_COUNTER: int = 100
 
 offset: int = -2
@@ -12,17 +17,36 @@ counter: int = 0
 chat_id: int
 
 
+offset: int = -2
+counter: int = 0
+cat_response: requests.Response
+cat_link: str
+numbers = [1, 2,3]
+
 while counter < MAX_COUNTER:
-
-    print('attempt =', counter)  #Чтобы видеть в консоли, что код живет
-
+    print('attempt =', counter)
     updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}').json()
 
     if updates['result']:
         for result in updates['result']:
             offset = result['update_id']
             chat_id = result['message']['from']['id']
-            requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={TEXT}')
+            a=random.choice(numbers)
+            if a==1:
+                cat_response = requests.get(API_CATS_URL)
+                cat_link = cat_response.json()[0]['url']
+            elif a==2:
+                cat_response = requests.get(API_WOOFS_URL)
+                cat_link = cat_response.json()['url']
+            else:
+                cat_response = requests.get(API_FOX_URL)
+                cat_link = cat_response.json()['image']
+            if cat_response.status_code == 200:
+
+                requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={TEXT}')
+                requests.get(f'{API_URL}{BOT_TOKEN}/sendPhoto?chat_id={chat_id}&photo={cat_link}')
+            else:
+                requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={ERROR_TEXT}')
 
     time.sleep(1)
     counter += 1
