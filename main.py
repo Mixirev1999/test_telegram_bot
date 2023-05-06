40d0667953bc689166c3c00e5db8fddbc5778bb2
Mikhirev Mikhail <mixirev99@gmail.com>
2023-05-06 22:46:08 +0300

    clean

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import math
 import requests
 import pprint
+import os
+import dotenv
+
 
 if __name__ == '__main__':
     api_url = 'https://api.telegram.org/bot6205359567:AAG3fGWI5-tSkkceRU6BgO4MZWncdesrBAk/sendMessage?chat_id=731057670&text=Ну ты и Миша!'
 
+    dotenv.load_dotenv()
+
+    print(os.getenv('BOT_TOKEN'))
+    print(os.getenv('ADMIN_ID'))
     response = requests.get(api_url)  # Отправляем GET-запрос и сохраняем ответ в переменной response
 
     if response.status_code == 200:  # Если код ответа на запрос - 200, то смотрим, что пришло в ответе
